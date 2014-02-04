35ed8f5fb8e4b22da7770be16b85dacc003bbf9d
Zhihao Yuan <zhihao.yuan@rackspace.com>
2014-02-04 18:05:12 -0500

    Detect archive type for old version git.

--- tools/archivef.sh
+++ tools/archivef.sh
@@ -4,8 +4,25 @@ set -e
 path_tmpl="$1"
 filename_tmpl=$(basename "$path_tmpl")
 dir_tmpl="${filename_tmpl%*.tar.*}"
+
+suffix="${filename_tmpl##*.tar.}"
+case "$suffix" in
+xz)
+	tool=xz
+	;;
+gz)
+	tool=gzip
+	;;
+bz2)
+	tool=bzip2
+	;;
+*)
+	echo "$0: Unknown suffix: $suffix"
+	exit 1
+esac
+
 rel=$(git describe --tags)
 path=$(printf "$path_tmpl" $rel)
 dir=$(printf "$dir_tmpl" $rel)
 
-git archive --prefix="$dir/" -o "$path" HEAD
+git archive --prefix="$dir/" HEAD | $tool > "$path"
